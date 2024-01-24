c2900a28857cef05a47e5f2de7ddc212f27508db
Ana Gelez <ana@gelez.xyz>
2024-01-24 13:22:47 +0100

    Remove unused devShell

--- flake.nix
+++ flake.nix
@@ -18,36 +18,13 @@
           (import ./packages/wartid.nix { inherit pkgs rust; }) //
           (import ./packages/wartapuretai.nix { inherit pkgs rust; }) //
           (import ./packages/wjtp.nix { inherit pkgs rust; });
-
-        defaultPackage = self.packages.${system}.wartid-server;
-        devShell = pkgs.mkShell {
-          name = "wartid";
-          buildInputs = with pkgs; [ fenix.packages.${system}.minimal.cargo fenix.packages.${system}.minimal.rustc diesel-cli postgresql ];
-          shellHook = ''
-            export PGDATA=$PWD/postgres_data
-            export PGHOST=$PWD/postgres
-            export LOG_PATH=$PWD/postgres/LOG
-            export PGDATABASE=wartid
-            export DATABASE_URL="postgresql:///wartid?host=$PGHOST"
-            if [ ! -d $PGHOST ]; then
-              mkdir -p $PGHOST
-            fi
-            if [ ! -d $PGDATA ]; then
-              echo 'Initializing postgresql database...'
-              initdb $PGDATA --auth=trust >/dev/null
-              pg_ctl start -l $LOG_PATH -o "-c listen_addresses= -k $PGHOST"
-              createuser -d wartid
-              createdb -O wartid wartid
-            else
-              pg_ctl start -l $LOG_PATH -o "-c listen_addresses= -k $PGHOST"
-            fi
-          '';
-        };
-      }) // {
-        nixosModule = { config, pkgs, lib, ... }:
-          (import ./services/milpertuis.nix { inherit config pkgs lib; }) //
-          (import ./services/wartid.nix { inherit config pkgs lib; }) // 
-          (import ./services/wartapuretai.nix { inherit config pkgs lib; }) //
-          (import ./services/wjtp.nix { inherit config pkgs lib; });
-  };
+      })
+    //
+    {
+      nixosModule = { config, pkgs, lib, ... }:
+        (import ./services/milpertuis.nix { inherit config pkgs lib; }) //
+        (import ./services/wartid.nix { inherit config pkgs lib; }) // 
+        (import ./services/wartapuretai.nix { inherit config pkgs lib; }) //
+        (import ./services/wjtp.nix { inherit config pkgs lib; });
+    };
 }
