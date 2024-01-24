41207040abed73c997d61306bb61b62703355593
Ana Gelez <ana@gelez.xyz>
2024-01-24 14:28:35 +0100

    Improve the milpertuis service to make it actually work

--- services/milpertuis.nix
+++ services/milpertuis.nix
@@ -1,18 +1,19 @@
 { config, pkgs, lib }:
 
 let
-  mpt = config.services.milpertuis;
-  milpertuisDir = cfg: pkgs.writeText "config.toml"
+  cfg = config.services.milpertuis;
+  configFile = cfg: pkgs.writeText "config.toml"
     ''
       base_url = 'https://${cfg.domain}'
       database_url = '${cfg.databaseUrl}'
       cookies_key = '${cfg.cookiesKey}'
       listen_on = '127.0.0.1:3838'
+      state_directory = '/var/lib/milpertuis'
 
       [mail]
-      address = '${cfg.mailServer}'
-      username = '${cfg.mailUser}'
-      password = '${cfg.mailPassword}'
+      address = '${cfg.mail.address}'
+      username = '${cfg.mail.user}'
+      password = '${cfg.mail.password}'
     '';
 in
 with lib;
@@ -32,36 +33,51 @@ with lib;
       type = types.str;
       description = "PostgreSQL database URL";
     };
+    # TODO: init it automatically if needed with a script
+    # See https://github.com/NixOS/nixpkgs/blob/nixos-23.11/nixos/modules/services/web-apps/mastodon.nix#L667
     cookiesKey = mkOption {
       type = types.str;
       description = "Key to encrypt secret cookies. Can be generated with `openssl rand -base64 32`";
     };
-    mailUser = mkOption {
-      type = types.str;
-      description = "Mail user name";
+    mail = {
+      user = mkOption {
+        type = types.str;
+        description = "Mail user name";
+      };
+      password = mkOption {
+        type = types.str;
+        description = "Mail password";
+      };
+      address = mkOption {
+        type = types.str;
+        description = "Mail server address";
+      };
     };
-    mailPassword = mkOption {
+    user = mkOption {
       type = types.str;
-      description = "Mail password";
+      description = "System user. If set to `milpertuis`, it will be created.";
     };
-    mailServer = mkOption {
+    group = mkOption {
       type = types.str;
-      description = "Mail server";
+      description = "System group. If set to `milpertuis`, it will be created.";
     };
   };
 
-  config.systemd.services.milpertuis = mkIf mpt.enable {
+  config.systemd.services.milpertuis = mkIf cfg.enable {
     description = "Milpertuis";
     wantedBy = [ "multi-user.target" ];
     after = [ "network.target" ];
     serviceConfig = {
-      WorkingDirectory = milpertuisDir mpt;
-      ExecStart = "${self.packages.${pkgs.system}.milpertuis}/bin/milpertuis";
+      User = cfg.user;
+      Group = cfg.group;
+      WorkingDirectory = self.packages.${pkgs.system}.milpertuis;
+      StateDirectory = "milpertuis";
+      ExecStart = "${self.packages.${pkgs.system}.milpertuis}/bin/milpertuis ${configFile cfg}";
       Type = "simple";
     };
   };
 
-  config.services.nginx.virtualHosts."${mpt.domain}" = mkIf mpt.enableNginx {
+  config.services.nginx.virtualHosts."${cfg.domain}" = mkIf cfg.enableNginx {
     enableACME = true;
     forceSSL = true;
     root = "${self.packages.${pkgs.system}.milpertuis}";
@@ -74,4 +90,12 @@ with lib;
       };
     };
   };
+
+  config.users.users.milpertuis = lib.mkIf (cfg.user == "milpertuis") {
+    isSystemUser = true;
+    home = self.packages.${pkgs.system}.milpertuis;
+    inherit (cfg) group;
+  };
+
+  users.groups.${cfg.group}.members = lib.optional cfg.enableNginx config.services.nginx.user;
 }
