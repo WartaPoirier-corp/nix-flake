35f9c1fe5c23011acac4b1ee4de8c16b5486b4ad
Ana Gelez <ana@gelez.xyz>
2024-01-26 18:20:57 +0100

    Update milpertuis

--- packages/milpertuis.nix
+++ packages/milpertuis.nix
@@ -2,15 +2,15 @@
 
 let
   milpertuisSrc = fetchTarball {
-    url = "https://wp-corp.eu.org/milpertuis-0.1.0-beta2.tar.gz";
-    sha256 = "0xy9d04iy8k0gj7ygq37jyzqhmpc992xpg7i1ngajc7i8hbmn2vh";
+    url = "https://wp-corp.eu.org/milpertuis-0.1.0-beta3.tar.gz";
+    sha256 = "1ib9wwf81ijgsc10cn5ml38zq925ah15zdpy611bw3mjx7xgdcyw";
   };
 in {
   milpertuis = pkgs.rustPlatform.buildRustPackage {
     pname = "milpertuis";
-    version = "0.1.0";
+    version = "0.1.0-beta3";
     src = milpertuisSrc;
-    cargoSha256 = "sha256-zYaXl0XYT+0aVtFFOnRzyixwuI5AiKUHP86bMdgECJc=";
+    cargoSha256 = "sha256-6Lh/aP+ZlUyTVDGZ4WCLzilimFHHql8CcDjBtXu7n+0=";
     doCheck = false;
     meta = with pkgs.lib; {
       description = "Pijul-based software forge";
